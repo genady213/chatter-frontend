d1df5ed5079c1570a1dc5b589eb29c8df27feda3
ChrisC7274 <chrischungyt@gmail.com>
2022-11-10 16:12:31 -0500

    -Added UI for Popup component when new message is clicked -Used reactjs popup

--- chatter-app/src/components/Sidebar/Sidebar.js
+++ chatter-app/src/components/Sidebar/Sidebar.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useCallback, setData } from "react"
+import Popup from 'reactjs-popup';
 import './Sidebar.css';
 import CreateIcon from '@mui/icons-material/Create';
 import SidebarOption from './SidebarOption';
@@ -6,6 +7,7 @@ import CommentIcon from '@mui/icons-material/Comment';
 import { useStateValue } from "../../StateProvider";
 import Cookies from "js-cookie";
 import apiClient from "../../apiClient";
+import TextField  from '@mui/material/TextField';
 
 
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
@@ -13,6 +15,7 @@ import AddIcon from "@material-ui/icons/Add"
 import LoopIcon from "@material-ui/icons/Loop"
 
 export function Sidebar() {
+
 
   const [{ user }] = useStateValue()
 	const [channels, setChannels] = useState([])
@@ -28,7 +31,7 @@ export function Sidebar() {
 		  console.log(error);
 		});
 	  
-	  }, [])
+	}, [])
 
 	useEffect(() => {
 		fetchData()
@@ -41,10 +44,34 @@ export function Sidebar() {
 	}, [channels])
 
   return (
-<div className="sidebar">
-			
-		<SidebarOption Icon={CommentIcon} title="New Message" /> 
-      		<hr />
+	  <div className="sidebar">
+
+
+		  {/*<SidebarOption Icon={CommentIcon} title="New Message"/>*/}
+
+		  <Popup
+			  trigger={<button className="button"> <CommentIcon/> New Message </button>}
+			  modal
+			  nested
+		  >
+			  {close => (
+				  <div className="modal">
+					  <button className="close" onClick={close}>
+						  &times;
+					  </button>
+					  <div className="header"> Chatter someone up! </div>
+					  <div className="textFieldArea">						  
+						  <TextField className="textFields" label="Conversation Name" />
+						  <TextField className="textFields" label="Who are you chatting with?" />
+						  <div className="popupButtonArea">
+							  <button className="popupButton">Start Chatting</button>
+						  </div>
+					  </div>
+				  </div>
+			  )}
+			  </Popup>
+
+		  <hr />
 
 			
 			{loading ||
@@ -56,6 +83,7 @@ export function Sidebar() {
 					/>
 					
 				))}
-		</div>
+
+		</div>	
   );
 }
