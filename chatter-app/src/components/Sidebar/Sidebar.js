c5ba74754d2a38d3289e5c9513b5d7306169ce34
ChrisC7274 <chrischungyt@gmail.com>
2022-11-10 17:20:50 -0500

    -Added ui for popup component -Used reactjs-popup dependency

--- chatter-app/src/components/Sidebar/Sidebar.js
+++ chatter-app/src/components/Sidebar/Sidebar.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useCallback, setData } from "react"
+import Popup from 'reactjs-popup';
 import './Sidebar.css';
 import CreateIcon from '@mui/icons-material/Create';
 import SidebarOption from './SidebarOption';
@@ -6,6 +7,7 @@ import CommentIcon from '@mui/icons-material/Comment';
 import { useStateValue } from "../../StateProvider";
 import Cookies from "js-cookie";
 import apiClient from "../../apiClient";
+import TextField from '@mui/material/TextField';
 
 
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
@@ -14,21 +16,22 @@ import LoopIcon from "@material-ui/icons/Loop"
 
 export function Sidebar() {
 
-  const [{ user }] = useStateValue()
+
+	const [{ user }] = useStateValue()
 	const [channels, setChannels] = useState([])
 	const [loading, setLoading] = useState("")
 
 	const fetchData = useCallback(async () => {
 		const data = await apiClient.get('/user/' + Cookies.get('userid')
-		, { headers: {"Authorization" : `${Cookies.get('token')}`} })
-		.then((response) => {
-			  console.log(response.data.conversations);
-			  setChannels(response.data.conversations);
-		}, (error) => {
-		  console.log(error);
-		});
-	  
-	  }, [])
+			, { headers: { "Authorization": `${Cookies.get('token')}` } })
+			.then((response) => {
+				console.log(response.data.conversations);
+				setChannels(response.data.conversations);
+			}, (error) => {
+				console.log(error);
+			});
+
+	}, [])
 
 	useEffect(() => {
 		fetchData()
@@ -40,13 +43,37 @@ export function Sidebar() {
 		else setLoading("")
 	}, [channels])
 
-  return (
-<div className="sidebar">
-			
-		<SidebarOption Icon={CommentIcon} title="New Message" /> 
-      		<hr />
+	return (
+		<div className="sidebar">
 
-			
+
+			{/*<SidebarOption Icon={CommentIcon} title="New Message"/>*/}
+
+			<Popup
+				trigger={<button className="button"> <CommentIcon /> New Message </button>}
+				modal
+				nested
+			>
+				{close => (
+					<div className="modal">
+						<button className="close" onClick={close}>
+							&times;
+						</button>
+						<div className="header"> Chatter someone up! </div>
+						<div className="textFieldArea">
+							<TextField className="textFields" label="Conversation Name" />
+							<TextField className="textFields" label="Who are you chatting with?" />
+							<div className="popupButtonArea">
+								<button className="popupButton">Start Chatting</button>
+							</div>
+						</div>
+					</div>
+				)}
+			</Popup>
+
+			<hr />
+
+
 			{loading ||
 				channels.map((channel) => (
 					<SidebarOption
@@ -54,8 +81,9 @@ export function Sidebar() {
 						title={channel.conversationName}
 						id={channel._id}
 					/>
-					
+
 				))}
+
 		</div>
-  );
+	);
 }
