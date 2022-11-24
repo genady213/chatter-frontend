2c456193e90ef5d4f987821974b85ea82ceb7afb
Chris Chung <114274146+ChrisC7274@users.noreply.github.com>
2022-11-24 18:38:06 -0500

    Update ChatInput.js

--- chatter-app/src/components/Middle/ChatInput.js
+++ chatter-app/src/components/Middle/ChatInput.js
@@ -52,7 +52,6 @@ export function ChatInput({ channelName, channelId }) {
   return (
     <div className="chatInput">
           <img className="sendIcon" src="/chatterLogoBlack.png" width="100" height="100" />
-          {/*<div className="chat_inputBackground">hi</div>*/}
       <form>
         <input
           value={input}
